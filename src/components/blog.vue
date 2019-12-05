94c8a483b6d89ea5ad2d27a2c25052ae692cdb1b
wangchunyu <18254880668@163.com>
2019-12-05 23:19:10 +0800

    updata

---
+++ src/components/blog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>博客列表</h1>
+    <ul>
+      <li v-for="(blog,key) in blogs" :key='id' :blog='title'></li>
+    </ul>
+  </div>
+  
+</template>
+<script>
+export default {
+  data:{
+    blogs:[
+      {id: 1, title: 'Blog1111'},
+      {id: 3, title: 'Blog3333'},
+      {id: 5, title: 'Blog5555'},
+    ]
+  },
+  components:{
+    Blog
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-console.log('Hello Webpack!')
-    document.getElementById('root').innerHTML = '<h1>Hello222</h1>'
+// import logo from './assets/imgs/logo.png'
+// import  './assets/css/my.css'
+
+// const image = new Image()
+// image.src = logo
+// document.body.appendChild(image)
+// document.getElementById('root').innerHTML = '<h1>Hello222</h1>'
+
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  el:'#root',
+  components:{
+    App:App
+  },
+  template:'<App/>'
+})
