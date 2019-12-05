24f7f7daba8cf2fef9ea0547352f77608ecd43cc
wangchunyu <18254880668@163.com>
2019-12-05 23:19:10 +0800

    updata component_test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   // 模式: 生产环境
@@ -16,7 +17,38 @@ module.exports = {
   // 模块加载器
   module: {
     rules: [
-
+      // 处理ES6
+      {
+        test: /\.js$/,
+        //exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      // 处理CSS
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'], // 多个loader从右到左处理
+      },
+      // 处理图片
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 1000,
+          name: 'static/img/[name].[hash:7].[ext]' // 相对于output.path
+        }
+      },
+      // 处理vue单文件组件模块
+      {
+        test: /\.vue$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'vue-loader'
+      }
     ]
   },
   // 插件
@@ -24,6 +56,22 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.html',//将那个页面作为模板页面处理（在根目录查找）
       filename: 'index.html'// 生成页面（在output指定的path下）
-    })
-  ]
+    }),
+    new VueLoaderPlugin()
+  ],
+  // 配置开发服务器
+  devServer: {
+    port:8080,
+    open: true, // 自动打开浏览器
+    quiet: true, // 不做太多日志输出
+  },
+  // 开启source-map调试
+  devtool: 'cheap-module-eval-source-map',
+  // 引入模块的解析
+  resolve: {
+    extensions: ['.js', '.vue', '.json'], // 可以省略的后缀名
+    alias: { // 路径别名(简写方式)
+      'vue$': 'vue/dist/vue.esm.js',  // 表示精准匹配
+    }
+  }
 }
